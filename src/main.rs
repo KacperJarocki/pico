4189ce174daff6ee964219d4653f1e8cb136500d
Kacper Jarocki <kacper@MacBook-Air-Kacper.local>
2023-10-19 11:20:02 +0200

    deleted comment

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
 #![no_std]
 #![no_main]
 
-// The macro for our start-up function
 use embedded_hal::digital::v2::{InputPin, OutputPin};
 use panic_halt as _;
 use rp_pico::entry;
@@ -12,11 +11,9 @@ use rp_pico::hal::Clock;
 fn main() -> ! {
     let mut is_locked: bool = false;
     let core = pac::CorePeripherals::take().unwrap();
-    // Grab our singleton objects
     let mut pac = pac::Peripherals::take().unwrap();
     let mut watchdog = hal::Watchdog::new(pac.WATCHDOG);
 
-    // The single-cycle I/O block controls our GPIO pins
     let sio = hal::Sio::new(pac.SIO);
     let clocks = hal::clocks::init_clocks_and_plls(
         rp_pico::XOSC_CRYSTAL_FREQ,
@@ -32,7 +29,6 @@ fn main() -> ! {
 
     let mut delay = cortex_m::delay::Delay::new(core.SYST, clocks.system_clock.freq().to_Hz());
 
-    // Set the pins up according to their function on this particular board
     let pins = rp_pico::Pins::new(
         pac.IO_BANK0,
         pac.PADS_BANK0,
@@ -40,7 +36,6 @@ fn main() -> ! {
         &mut pac.RESETS,
     );
 
-    // Our LED output
     let mut led_pin_locked = pins
         .gpio26
         .into_push_pull_output_in_state(hal::gpio::PinState::Low);
